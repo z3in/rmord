c185ffd33dfb407b671c581ef60b5669bedaad5a
z3in <sebaschianrobin@gmail.com>
2021-12-10 10:46:50 +0800

    change in credentals

--- admin/includes/connect.php
+++ admin/includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 /* Database config */
-    $db_host		= '127.0.0.1';
-    $db_user		= 'remote';
-    $db_pass		= 'TUBoYWJhaGFiYW5nUEA1NXcwcmQxdDAh';
+    $db_host		= 'localhost';
+    $db_user		= 'root';
+    $db_pass		= '';
     $db_database	= 'oroars';
 $production = true;
 /* End config */
