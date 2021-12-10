e33e96fa11263fd88919d16c14fc0a0c5f6b8521
z3in <sebaschianrobin@gmail.com>
2021-12-10 10:28:32 +0800

    change database credentials

--- admin/includes/connect.php
+++ admin/includes/connect.php
@@ -17,6 +17,4 @@ if($production){
     $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-
 ?>
